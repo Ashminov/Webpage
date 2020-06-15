feea4db33fac354800ad4239e9593c208edec316
Dimitar Ashminov <46502028+Ashminov@users.noreply.github.com>
2020-06-15 22:25:59 +0300

    Add files via upload

---
+++ JavaScript files/project.js
@@ -0,0 +1,162 @@
+
+
+function emain1()
+{
+
+    if(window.screen.width>'500')
+    {
+        window.scrollTo('0','850');
+
+    }
+    else
+    {
+        window.scrollTo('0','1350');
+
+    }
+}
+
+function emain2()
+{
+    if(window.screen.width>'500')
+    {
+        window.scrollTo('0','1500');
+
+    }
+else
+    {
+        window.scrollTo('0','1920');
+
+    }
+}
+function emain3()
+{
+
+    if(window.screen.width>'500')
+    {
+        window.scrollTo('0','2360');
+
+
+    }
+    else
+    {
+        window.scrollTo('0','2620');
+
+    }
+
+}
+function emain4()
+{
+    window.scrollTo('0','3580');
+
+}
+
+        function fmain1()
+        {
+
+            if(window.screen.width>'500')
+            {
+                window.scrollTo('0','870');
+
+            }
+            else
+            {
+                window.scrollTo('0','1150');
+
+            }
+
+        }
+
+        function fmain2()
+        {
+
+            if(window.screen.width>'500')
+            {
+                window.scrollTo('0','1470');
+            }
+            else
+            {
+                window.scrollTo('0','1900');
+
+            }
+
+
+        }
+
+        function fmain3()
+        {
+            if(window.screen.width>'500')
+            {
+                window.scrollTo('0','2360');
+            }
+            else
+            {
+                window.scrollTo('0','2400');
+
+            }
+
+        }
+
+        function fmain5()
+        {
+            window.scrollTo('0','3400');
+        }
+
+
+        function sh()
+    {
+        if( document.getElementById('non').style.display==='none')
+        {
+            document.getElementById('non').style.display='block';
+
+        }
+        else
+        {
+            document.getElementById('non').style.display='none'
+        }
+
+    }
+
+    function to()
+    {
+                window.scrollTo('0','1');
+    }
+    function tr()
+    {
+            window.open("bgproject.html","_top");
+    }
+
+    function tr1()
+    {
+        window.open("project.html","_top");
+    }
+
+
+function wt() {
+    document.getElementById('M').innerHTML = 'Grigor Dimitrov (Bulgarian: Григор Димитров, pronounced [ɡriˈɡɔr dimiˈtrɔf]; born 16 May 1991) ' +
+        'is a Bulgarian professional tennis player. His career-high ATP singles ranking is world No. 3, which he achieved in November 2017 after winning ' +
+        'the ATP Finals.\n' +
+        '\n' +
+        'Dimitrov is the most successful Bulgarian male tennis player to date, both in terms of ranking reached —as of 2017 he is the only Bulgarian tennis' +
+        ' player ever to be ranked inside the top ten ATP singles— and prize money won, being the only male Bulgarian player to reach US$1m[2] and later $18m.\n' +
+        '\n' +
+        'Dimitrov is also the first (and only) Bulgarian male tennis player to win an ATP title in singles (after he won in October 2013 at Stockholm) ' +
+        'and reach a final in doubles (in 2011) as well as the only Bulgarian male player to reach the third round or better at a Grand Slam tournament.' +
+        ' Dimitrov is also the first Bulgarian to qualify for, and later win, the ATP Finals.[3] He has won eight ATP singles titles to date.\n' +
+        '\n' +
+        'Prior to his professional career, Dimitrov enjoyed a successful junior career, in which he held the world No. 1 ranking and won consecutive grand ' +
+        'slam boys\' singles titles at the 2008 Wimbledon Championships and the 2008 US Open— at the former he defeated future men\'s doubles No. 1 player Henri ' +
+        'Kontinen in the final. After returning from injuries, Dimitrov beat Roger Federer for the first time in his career in five sets at the 2019 US Open,' +
+        ' before losing to Daniil Medvedev in the semifinals.';
+}
+
+        function t2() {
+            document.getElementById('MBG').innerHTML = 'Григор Димитров е български тенисист. ' +
+                'Той е носител на шампионските титли на Уимбълдън и Откритото първенство на САЩ през 2008 г. за юноши. ' +
+                'В професионалната си кариера е спечелил 8 титли от Световния Тур на ATP (Асоциацията на професионалните тенисисти), като' +
+                ' най-значимите от тях са през 2017 г. на турнирите от финалния Мастърс в Лондон и Мастърс 1000 в Синсинати.\n' +
+                '\n' +
+                'За първи път се изкачва в Топ 10 на световната ранглиста на 7 юли 2014 г. На 20 ноември 2017 г' +
+                '. достига до своето върхово постижение (3-то място) и остава на тази позиция 10 последователни седмици.' +
+                ' В периода юли 2014 г. – ноември 2018 г. Григор Димитров е бил общо 81 седмици сред най-добрите десет тенисисти' +
+                ' в света, от които 63 последователни (21 август 2017 г. – 4 ноември 2018 г.).';
+        }
